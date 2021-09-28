569090b6c4dcf7dd673190acb6599aa9b0ccd834
tomasvaro <tomas.varo63@gmail.com>
2021-09-28 12:49:28 +0200

    Added random Secret Word.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -9,6 +9,9 @@ namespace Hangman
             string[] words = {"Soffa", "Blåbär", "Gummi", "Trevlig", "Under", "Läser", "Programmering", "Lista", "Slumpmässigt", "Sätter",
                 "Inlevelse", "Beskrivning", "Veckoslut", "Vinterdäck", "Långfärdsskridskor", "Grävutrustning", "Sparkcykel", "Norrsken", "Höstfärger", "Mandelblom",
                 "Längtan", "Pensionerad", "Gammal", "Filosofisk", "Huvudled", "Riktlinjer", "Verkstad", "Vommen", "Ordvits", "Bohusleden"};
+            Random rand = new Random();
+            int index = rand.Next(words.Length);
+            string secretWord = words[index];
         }
     }
 }
